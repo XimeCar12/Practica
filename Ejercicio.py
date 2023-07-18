f019dc3a560e90ae14163a110d15b21ff594c349
XimeCar12 <xcarpiojuarez@gmail.com>
2023-07-18 09:51:32 -0600

    Eres mayor de edad o menor

--- Ejercicio.py
+++ Ejercicio.py
@@ -2,3 +2,7 @@ print("Hola mundo")
 
 print(input("Ingresa tu nombre: "))
 print(int("Ingresa tu edad: "))
+if(edad>=18):
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")    
